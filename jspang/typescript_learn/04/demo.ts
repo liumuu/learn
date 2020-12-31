cdf482e13f94eb58cb4c2dac16fb2a69957162f7
liumuu <liumuu@163.com>
2020-12-31 23:01:53 +0800

    ts 04

---
+++ jspang/typescript_learn/04/demo.ts
@@ -0,0 +1,16 @@
+let count: number
+count = 123
+
+let countInference = 123
+
+const one = 1
+const two = 2
+const three = one + two
+
+function getTotal(one, two) {
+  return one + two
+}
+
+const total = getTotal(1, 2)
+
+console.log(total)
