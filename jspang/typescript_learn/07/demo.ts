e8e5dea85762eab6304b981ec60e8330e8e036d2
liumuu <liumuu@163.com>
2020-12-31 23:49:06 +0800

    ts 07

---
+++ jspang/typescript_learn/07/demo.ts
@@ -0,0 +1,6 @@
+const girls: [string, string, number] = ['xiaohong', 'xiaolan', 18]
+const girlsArr: [string, string, number][] = [
+  ['xiaohong', 'teacher', 18],
+  ['xiaolan', 'teacher', 22],
+  ['xiaohui', 'student', 12],
+]
